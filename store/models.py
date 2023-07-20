b2a912b2857e581fb899859a1f5f95b5f9869fc2
velkods <ranon.deguzman@obf.ateneo.edu>
2023-07-20 19:18:14 +0800

    Product database added; database not yet linked to products page.

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    slug = models.CharField(max_length=120)
+    title = models.CharField('Product Name', max_length=120)
+    image = models.FileField()
+    price = models.FloatField('Price')
+    description = models.TextField(blank=True)
+    availability = models.BooleanField()
+    date = models.DateTimeField('Release Date')
+
+    def __str__(self):
+        return self.title   
